5b3aec288d8bbc793ea9e98b90530672260e1e13
Jack Herrington <jherr@pobox.com>
2020-12-06 17:45:43 -0800

    First commit

---
+++ extension/src/plugin/WelcomePageRoute.plugin.js
@@ -0,0 +1,7 @@
+export default {
+    "ScandipwaTest-App-2/Component/WelcomePage/Component/WelcomePageComponent": {
+        "member-function": {
+            renderHello: () => "Goodbye - That is cool",
+        },
+    },
+};
